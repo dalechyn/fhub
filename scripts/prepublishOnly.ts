091a85d8e8f34027955fedde94e82464261f6db8
dalechyn <vlad.dalechin@gmail.com>
2024-10-05 22:43:44 +0300

    feat: init

---
+++ scripts/prepublishOnly.ts
@@ -0,0 +1,11 @@
+// Taken from `viem`: https://github.com/wevm/viem/blob/main/scripts/prepublishOnly.ts
+import { join } from 'node:path'
+
+const packageJsonPath = join(import.meta.dir, '../src/package.json')
+const packageJson = await Bun.file(packageJsonPath).json()
+
+// NOTE: We explicitly don't want to publish the type field.
+// We create a separate package.json for `dist/cjs` and `dist/esm` that has the type field.
+delete packageJson.type
+
+Bun.write(packageJsonPath, JSON.stringify(packageJson, null, 2))
