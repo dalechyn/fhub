715fb81307973115b390736b032b47e1162faf29
dalechyn <vlad.dalechin@gmail.com>
2024-11-03 19:38:40 +0200

    feat: resolve mentions in `getCast`

--- src/Internal/Actions/Cast/getCast.ts
+++ src/Internal/Actions/Cast/getCast.ts
@@ -12,6 +12,9 @@ export declare namespace Actions_Cast_getCast {
   type ReturnType = NodeTypes.Cast & {
     recasts: NodeTypes.Reaction[]
     likes: NodeTypes.Reaction[]
+    text: {
+      formatted: string
+    }
   }
   type ErrorType = NodeActions.Cast.getCast.ErrorType | GlobalErrorType
 }
@@ -40,8 +43,25 @@ export async function Actions_Cast_getCast(
   const likes = reactions.filter((reaction) => reaction.type === 'like')
   const recasts = reactions.filter((reaction) => reaction.type === 'recast')
 
+  const formattedText = await (async () => {
+    let text = cast.text.value
+    if (!cast.text.mentions) return text
+    for (const mention of cast.text.mentions.reverse()) {
+      const mentionUsername = await NodeActions.UserData.getUserDataUsername(
+        client,
+        { fid: mention.fid },
+      )
+      text = `${text.slice(0, mention.position)}@${mentionUsername}${text.slice(mention.position)}`
+    }
+    return text
+  })()
+
   return {
     ...cast,
+    text: {
+      ...cast.text,
+      formatted: formattedText,
+    },
     likes,
     recasts,
   }
