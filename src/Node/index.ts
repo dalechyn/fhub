767e6ad85b239e3be0b892158342d3e62f4d9bd5
dalechyn <vlad.dalechin@gmail.com>
2024-11-07 23:04:08 +0200

    nit: use server pragmas

--- src/Node/index.ts
+++ src/Node/index.ts
@@ -1,3 +1,4 @@
+'use server'
 export * as Actions from './Actions.js'
 export * as Types from './Types.js'
 export * as Constants from './Constants.js'
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+'use server'
 export * as Actions from './Actions.js'
 export * as Client from './Client.js'
 export * as Transport from './Transport.js'
